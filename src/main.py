9863e9964f96e6fcd9c5d1b6f29f81cc0b08fda4
Karahan Buhan <contact@karahanbuhan.com>
2026-07-07 10:25:58 +0300

    Format main.py, use autopep8 from now on to parse

--- src/main.py
+++ src/main.py
@@ -22,10 +22,11 @@ origins = [
     "http://localhost:8000",
 ]
 
+
 @app.on_event("startup")
 async def database_connect():
     await database.connect()
-    
+
     # Create events table if it does not exist
     query = """CREATE TABLE IF NOT EXISTS events (
                     id INTEGER PRIMARY KEY,
@@ -34,40 +35,49 @@ async def database_connect():
                     event_type TEXT NOT NULL,
                     username TEXT
                 )"""
-    await database.execute(query);
-    
+    await database.execute(query)
+
+
 @app.on_event("shutdown")
 async def database_disconnect():
     await database.disconnect()
 
+
 def generate_username():
     characters = string.ascii_letters + string.digits + '._-'
-    username = ''.join(random.choice(characters) for _ in range(random.randint(5, 32)))
+    username = ''.join(random.choice(characters)
+                       for _ in range(random.randint(5, 32)))
     return username
+
 
 @app.on_event("startup")
 @repeat_every(seconds=2)
 async def insert_mock_event():
-    timestamp = datetime.datetime.now(datetime.timezone.utc).replace(microsecond=0).isoformat()
-    source_ip = '{}.{}.{}.{}'.format(*__import__('random').sample(range(0,255),4)) # TODO: Generate IPv4s from specified classes
-    event_type = random.choice(["LOGIN_FAILED", "LOGIN_SUCCESS", "HIGH_CPU", "REQUEST"])
+    timestamp = datetime.datetime.now(
+        datetime.timezone.utc).replace(microsecond=0).isoformat()
+    # TODO: Generate IPv4s from specified classes
+    source_ip = '{}.{}.{}.{}'.format(
+        *__import__('random').sample(range(0, 255), 4))
+    event_type = random.choice(
+        ["LOGIN_FAILED", "LOGIN_SUCCESS", "HIGH_CPU", "REQUEST"])
 
     username = None
     if event_type.startswith("LOGIN"):
-        username = random.choice(["admin", "karahan", "ahmet", generate_username()])
-        
+        username = random.choice(
+            ["admin", "karahan", "ahmet", generate_username()])
+
     query = "INSERT INTO events (timestamp, source_ip, event_type, username) VALUES(:timestamp, :source_ip, :event_type, :username)"
     values = [
-        { 
-         "timestamp": timestamp,
-         "source_ip": source_ip,
-         "event_type": event_type,
-         "username": username
+        {
+            "timestamp": timestamp,
+            "source_ip": source_ip,
+            "event_type": event_type,
+            "username": username
         }
     ]
-    
+
     await database.execute_many(query=query, values=values)
-    
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -76,24 +86,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/api/events")
-def api_events():
-    return json.loads("""[
-    {
-        "id": 0,
-        "timestamp": "2026-07-06T14:32:10Z",
-        "sourceIp": "185.23.11.4",
-        "eventType": "LOGIN_FAILED",
-        "username": "admin"
-    },
-    {
-        "id": 1,
-        "timestamp": "2026-07-06T14:32:08Z",
-        "sourceIp": "91.44.10.2",
-        "eventType": "REQUEST",
-        "username": null
-    }
-]""")
+async def api_events():
+    query = "SELECT * FROM events"
+    results = await database.fetch_all(query=query)
+    return results
 
 
 @app.get("/api/alerts")
